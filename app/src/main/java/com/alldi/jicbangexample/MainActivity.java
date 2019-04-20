ff787dcec51d094076f6baa6aa9c945a76a81a5f
alldifferent <alldifferent@naver.com>
2019-04-20 14:04:27 +0900

    Initial commit

---
+++ app/src/main/java/com/alldi/jicbangexample/MainActivity.java
@@ -0,0 +1,40 @@
+package com.alldi.jicbangexample;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alldi.jicbangexample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding act;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        act.numTxt.setText("32개");
+        bindViews();
+        setupEvents();
+        setValues();
+
+    }
+    void setupEvents(){
+
+
+    }
+
+    void setValues(){
+
+
+    }
+
+    void bindViews(){
+
+
+    }
+
+
+}
