65adb4f42a9fabcbeb7e2122927b1457fdbbb663
alldifferent <alldifferent@naver.com>
2019-04-20 14:16:43 +0900

    임대 가능한 방에 대한 정보를 담는 클래스 생성.

---
+++ app/src/main/java/com/alldi/jicbangexample/datas/Room.java
@@ -0,0 +1,18 @@
+package com.alldi.jicbangexample.datas;
+
+public class Room {
+
+    int deposit; // 보증금
+    int month_pay // 월세
+    String loaction; //지역
+    int floor; //몇층 만약 지하라면 음수
+    String description; //설명
+
+    public Room(int deposit, int month_pay, String loaction, int floor, String description) {
+        this.deposit = deposit;
+        this.month_pay = month_pay;
+        this.loaction = loaction;
+        this.floor = floor;
+        this.description = description;
+    }
+}
