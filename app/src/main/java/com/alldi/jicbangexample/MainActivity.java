528ffa2a348a7ce183c03af59e087b384368f8e2
alldifferent <alldifferent@naver.com>
2019-04-20 15:51:38 +0900

    mainactivity에 어댑터 연결

--- app/src/main/java/com/alldi/jicbangexample/MainActivity.java
+++ app/src/main/java/com/alldi/jicbangexample/MainActivity.java
@@ -4,6 +4,7 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alldi.jicbangexample.adapters.RoomAdapter;
 import com.alldi.jicbangexample.databinding.ActivityMainBinding;
 import com.alldi.jicbangexample.datas.Room;
 
@@ -12,6 +13,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    RoomAdapter mRoomAdapter;
     List<Room> roomList = new ArrayList<>();
     ActivityMainBinding act;
 
@@ -20,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         act = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        act.numTxt.setText("32개");
         bindViews();
         setupEvents();
         setValues();
@@ -36,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
         roomList.add(new Room(500,35,"남구 둔산동", 2, "시세대비 저렴"));
         roomList.add(new Room(500,40,"서구 둔산동", 15, "리치빌 오피스 15평형"));
         roomList.add(new Room(500,35,"북구 둔산동", 15, "시청역 부근"));
+        roomList.add(new Room(500,35,"서구 둔산동", 7, "타임월드 인근"));
+        roomList.add(new Room(500,45,"동구 둔산동", 3, "갤러리 백화점 부근"));
+        roomList.add(new Room(500,35,"북구 둔산동", 20, "둔산동 아너스빌"));
+        roomList.add(new Room(500,35,"남구 둔산동", 2, "시세대비 저렴"));
+        roomList.add(new Room(500,40,"서구 둔산동", 15, "리치빌 오피스 15평형"));
     }
 
     void setupEvents(){
@@ -45,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
 
     void setValues(){
 
+        mRoomAdapter = new RoomAdapter(MainActivity.this, roomList);
+        act.roomListView.setAdapter(mRoomAdapter);
 
     }
 
