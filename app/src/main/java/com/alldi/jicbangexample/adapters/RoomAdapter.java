475b2df14cdb6d7ba2422413926b5687901dabc6
alldifferent <alldifferent@naver.com>
2019-04-20 15:44:32 +0900

    1) 데이터 객체 추출 2) 리스트아이템XML <-> Adapter클래스 자바 파일 연결 3) 데이터 들어있는 값을 적절한 자리에 출력

--- app/src/main/java/com/alldi/jicbangexample/adapters/RoomAdapter.java
+++ app/src/main/java/com/alldi/jicbangexample/adapters/RoomAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.alldi.jicbangexample.R;
 import com.alldi.jicbangexample.datas.Room;
@@ -16,6 +17,7 @@ public class RoomAdapter extends ArrayAdapter<Room> {
     Context mContext;
     List<Room> mList;
     LayoutInflater inf;
+
 
     public RoomAdapter(Context context, List<Room> list){
         super(context, R.layout.room_list_item, list);
@@ -31,9 +33,18 @@ public class RoomAdapter extends ArrayAdapter<Room> {
         View row = convertView;
         if (row == null){
             row = inf.inflate(R.layout.room_list_item, null);
+
         }
 
+        Room roomData = mList.get(position);
 
+        TextView paymemtTxt = row.findViewById(R.id.paymentTxt);
+        TextView localTxt = row.findViewById(R.id.localTxt);
+        TextView contextTxt = row.findViewById(R.id.contextTxt);
+
+        paymemtTxt.setText(String.format("%d/%d", roomData.deposit,roomData.month_pay));
+        localTxt.setText(String.format("%s, %d층",roomData.loaction,roomData.floor));
+        contextTxt.setText(roomData.description);
 
         return row;
 
--- app/src/main/java/com/alldi/jicbangexample/datas/Room.java
+++ app/src/main/java/com/alldi/jicbangexample/datas/Room.java
@@ -2,11 +2,11 @@ package com.alldi.jicbangexample.datas;
 
 public class Room {
 
-    int deposit; // 보증금
-    int month_pay; // 월세
-    String loaction; //지역
-    int floor; //몇층 만약 지하라면 음수
-    String description; //설명
+    public int deposit; // 보증금
+    public int month_pay; // 월세
+    public String loaction; //지역
+    public int floor; //몇층 만약 지하라면 음수
+    public String description; //설명
 
     public Room(int deposit, int month_pay, String loaction, int floor, String description) {
         this.deposit = deposit;
